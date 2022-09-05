768c36b78069f0148e2823968efff0f74e20dbc0
Dong thanh Tran <dtran@f0r1s9.codam.nl>
2022-09-05 17:19:11 +0200

    minishell.h and main concept

--- include/minishell.h
+++ include/minishell.h
@@ -6,56 +6,67 @@
 /*   By: dtran <dtran@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/08/31 15:57:39 by dtran         #+#    #+#                 */
-/*   Updated: 2022/09/04 16:30:55 by dtran         ########   odam.nl         */
+/*   Updated: 2022/09/05 17:18:21 by dtran         ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
-#define MINISHELL_H
+# define MINISHELL_H
 
 // Includes
-#include <readline/readline.h>
-#include <libft.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdbool.h>
+# include <libft.h>
+
+t_shelly	g_shelly;
+
+typedef struct s_env_var {
+	char				*key;
+	char				*value;
+	struct s_env_var	*next;
+}	t_env_var;
 
 // Token definitions
-typedef enum e_token_type
-{
-	PIPE = 1,
-	READ_IN = 2,
-	READ_OUT = 3,
-	DOLLAR = 4,
-	QUOTE2 = 5,
-	QUOTE = 6,
-	SPACE = 7,
-	TAB = 8,
-	NEWLINE = 9,
-	HERE_DOC = 10,
-	READ_OUT_APP = 11,
-	WORD = 12,
-	START = 13
-} t_token_type;
+typedef enum e_tokentype {
+	WORD = 1,
+	OPTION = 2,
+	LESS = 3,
+	GREAT = 4,
+	PIPE = 5,
+	AMPERSAND = 6,
+	NEWLINE = 7,
+	HEREDOC = 8,
+	GREATAMP = 9,
+	COMMAND = 10,
+	INFILE = 11,
+	OUTFILE = 12,
+}	t_tokentype;
 
-typedef struct s_app_data
-{
-	char	*env;
-	int		env_lines;
-}
+typedef struct s_token {
+	t_tokentype		type;
+	unsigned int	index;
+	unsigned int	length;
+	bool			expandable;
+	bool			adjacent;
+	struct s_token	*next;
+}	t_token;
 
-// typedef struct s_command
-// {
-// 	char	*infile;
-// 	char	*outfile;
-// 	char	**command;
-// 	char	*here_doc;
-// 	int		num;
-// 	char	*line;
-// } t_command;
+typedef struct s_command {
+	char	*path;
+	char	**args;
+}	t_command;
 
-typedef struct s_token
-{
-	t_token_type	token;
-	char			*value;
-} t_token;
+typedef struct s_shelly {
+	t_env_var		*env;
+	int				exit_code;
+	int				fd_in;
+	int				fd_out;
+	int				pipe[2];
+	pid_t			pid;
+	t_command		*cmds;
+	size_t			cmd_n;
+}	t_shelly;
 
 #endif
 
